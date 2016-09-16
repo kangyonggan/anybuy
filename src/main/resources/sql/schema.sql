0347c8ff5985fa6a371e31368aeef7083b6b687e
“houbb” <1060732496@qq.com>
2016-09-16 20:37:47 +0800

    基础表创建

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -4,40 +4,117 @@ CREATE DATABASE anybuy
 
 USE anybuy;
 
-CREATE TABLE demo
-(
-  id           BIGINT(20) PRIMARY KEY  AUTO_INCREMENT NOT NULL
-  COMMENT '主键, 自增',
-  is_deleted   TINYINT                                NOT NULL         DEFAULT 0
+CREATE TABLE `user` (
+  id           BIGINT(20) AUTO_INCREMENT NOT NULL PRIMARY KEY
+  COMMENT '主键,自增',
+  mobile       VARCHAR(20)               NOT NULL
+  COMMENT '手机号',
+  password     VARCHAR(64)               NOT NULL
+  COMMENT '密码',
+  nickname     VARCHAR(32)               NOT NULL         DEFAULT ''
+  COMMENT '昵称',
+  is_deleted   TINYINT                   NOT NULL         DEFAULT 0
   COMMENT '是否删除 {0:未删除, 1:已删除}',
-  created_time DATETIME                               NOT NULL
+  created_time DATETIME                  NOT NULL
   COMMENT '创建时间',
-  updated_time DATETIME                               NOT NULL
+  updated_time DATETIME                  NOT NULL
   COMMENT '最后更新时间'
 )
-  COMMENT '示例表';
-CREATE UNIQUE INDEX id_UNIQUE ON demo (id);
+  COMMENT '用户表';
+CREATE UNIQUE INDEX id_UNIQUE
+  ON `user` (id);
+CREATE UNIQUE INDEX mobile_UNIQUE
+  ON `user` (`mobile`);
 
 
-CREATE TABLE `user`(
-  id BIGINT(20) AUTO_INCREMENT NOT NULL PRIMARY KEY COMMENT '主键,自增',
-  mobile VARCHAR(20) NOT NULL COMMENT '手机号',
-  password VARCHAR(64) NOT NULL COMMENT '密码',
-  nickname VARCHAR(32) NOT NULL DEFAULT '' COMMENT '昵称'
-) COMMENT '用户表';
-CREATE UNIQUE INDEX id_UNIQUE ON `user` (id);
+CREATE TABLE `user_info` (
+  id            BIGINT(20) AUTO_INCREMENT NOT NULL PRIMARY KEY
+  COMMENT '主键,自增',
+  identity_card CHAR(18)                  NOT NULL
+  COMMENT '身份证',
+  real_name     VARCHAR(10)               NOT NULL
+  COMMENT '真是姓名',
+  is_deleted    TINYINT                   NOT NULL         DEFAULT 0
+  COMMENT '是否删除 {0:未删除, 1:已删除}',
+  created_time  DATETIME                  NOT NULL
+  COMMENT '创建时间',
+  updated_time  DATETIME                  NOT NULL
+  COMMENT '最后更新时间'
+)
+  COMMENT '用户信息表';
+
+CREATE UNIQUE INDEX id_UNIQUE
+  ON `user_info` (id);
+CREATE UNIQUE INDEX identity_card_UNIQUE
+  ON `user_info` (`identity_card`);
 
 
+CREATE TABLE 'address' (
+  id           BIGINT(20) AUTO_INCREMENT NOT NULL PRIMARY KEY
+  COMMENT '主键,自增',
+  post_code    CHAR(6)                   NOT NULL
+  COMMENT '邮编',
+  username     VARCHAR(20)               NOT NULL
+  COMMENT '收件人姓名',
+  mobile       VARCHAR(20)               NOT NULL
+  COMMENT '手机号',
+  address      VARCHAR(128)              NOT NULL
+  COMMENT '收货地址',
+  is_default   TINYINT                   NOT NULL         DEFAULT 0
+  COMMENT '是否默认',
+  user_id      BIGINT(20)                NOT NULL
+  COMMENT '用户ID',
+  is_deleted   TINYINT                   NOT NULL         DEFAULT 0
+  COMMENT '是否删除 {0:未删除, 1:已删除}',
+  created_time DATETIME                  NOT NULL
+  COMMENT '创建时间',
+  updated_time DATETIME                  NOT NULL
+  COMMENT '最后更新时间'
+)
+  COMMENT '收货地址表';
+
+CREATE UNIQUE INDEX id_UNIQUE
+  ON address (id);
 
 
-CREATE TABLE 'address'(
-  id BIGINT(20) AUTO_INCREMENT NOT NULL PRIMARY KEY COMMENT '主键,自增',
-  post_code CHAR(6) NOT NULL COMMENT '邮编',
-  username VARCHAR(20) NOT NULL COMMENT '收件人姓名',
-  mobile VARCHAR(20) NOT NULL COMMENT '手机号',
-  address VARCHAR(128) NOT NULL COMMENT '收货地址',
-  is_default TINYINT NOT NULL DEFAULT 0 COMMENT '是否默认',
-  user_id BIGINT(20) NOT NULL COMMENT '用户ID'
-) COMMENT '收货地址表';
+CREATE TABLE `card_history` (
+  id           BIGINT(20) AUTO_INCREMENT NOT NULL PRIMARY KEY
+  COMMENT '主键,自增',
+  card_no      VARCHAR(64)               NOT NULL
+  COMMENT '银行卡号',
+  bank_no      VARCHAR(32)               NOT NULL
+  COMMENT '银行编号',
+  user_id      BIGINT(20)                NOT NULL
+  COMMENT '用户ID',
+  is_deleted   TINYINT                   NOT NULL         DEFAULT 0
+  COMMENT '是否删除 {0:未删除, 1:已删除}',
+  created_time DATETIME                  NOT NULL
+  COMMENT '创建时间',
+  updated_time DATETIME                  NOT NULL
+  COMMENT '最后更新时间'
+)
+  COMMENT '绑卡记录';
+CREATE UNIQUE INDEX id_UNIQUE
+  ON `card_history` (id);
 
-CREATE UNIQUE INDEX id_UNIQUE ON address (id);
+
+CREATE TABLE `bank` (
+  id           BIGINT(20) AUTO_INCREMENT NOT NULL PRIMARY KEY
+  COMMENT '主键,自增',
+  bank_no      VARCHAR(64)               NOT NULL
+  COMMENT '银行编号',
+  bank_name    VARCHAR(64)               NOT NULL
+  COMMENT '银行名称',
+  is_deleted   TINYINT                   NOT NULL         DEFAULT 0
+  COMMENT '是否删除 {0:未删除, 1:已删除}',
+  created_time DATETIME                  NOT NULL
+  COMMENT '创建时间',
+  updated_time DATETIME                  NOT NULL
+  COMMENT '最后更新时间'
+)
+  COMMENT '银行表';
+CREATE UNIQUE INDEX id_UNIQUE
+  ON `bank` (id);
+
+CREATE UNIQUE INDEX bank_no_UNIQUE
+  ON `bank` (bank_no);
